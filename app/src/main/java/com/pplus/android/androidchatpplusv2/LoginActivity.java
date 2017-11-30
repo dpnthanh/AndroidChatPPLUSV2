b3de15e23fc7aa4f86ad5a09f6c4e90e6f032209
dang pham nhat thanh <dpnthanh@gmail.com>
2017-11-30 10:53:55 +0700

    i forgot what i did in yesterday

--- app/src/main/java/com/pplus/android/androidchatpplusv2/LoginActivity.java
+++ app/src/main/java/com/pplus/android/androidchatpplusv2/LoginActivity.java
@@ -1,13 +1,56 @@
 package com.pplus.android.androidchatpplusv2;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText edtName;
+    Button btnJoin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initControls();
+        initEvents();
     }
+
+    private void initEvents() {
+        btnJoin.setOnClickListener(this);
+    }
+
+    private void initControls() {
+        edtName = (EditText) findViewById(R.id.edtName);
+        btnJoin = (Button) findViewById(R.id.btnLogin);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnLogin:
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("name", edtName.getText().toString().trim());
+                intent.putExtras(bundle);
+                startActivity(intent);
+                break;
+
+        }
+    }
+
 }
--- app/src/main/java/com/pplus/android/androidchatpplusv2/MainActivity.java
+++ app/src/main/java/com/pplus/android/androidchatpplusv2/MainActivity.java
@@ -1,13 +1,17 @@
 package com.pplus.android.androidchatpplusv2;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -15,16 +19,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     String TAG = "socket";
     Socket socket = new Socket();
-    TextView txtMess;
+    TextView txtName;
     EditText edtMess;
     Button btnSubmit;
     BufferedReader is;
     BufferedWriter os;
+    String name = "Default user";
+    ListView lvMess;
+    ArrayList<String> lsMess = new ArrayList<>();
+    ArrayAdapter adapter;
 
 
     @Override
@@ -40,84 +49,73 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void initEvents() {
         btnSubmit.setOnClickListener(this);
+        txtName.setOnClickListener(this);
     }
 
     private void initDisplay() {
-
+        txtName.setText(name);
     }
 
     private void initControls() {
-//
-//        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-//        StrictMode.setThreadPolicy(policy);
-
         mapped();
-        new connect().execute();
 
+//        new connect().execute();
 
-//        if (ContextCompat.checkSelfPermission(getApplicationContext(),
-//                Manifest.permission.INTERNET)
-//                != PackageManager.PERMISSION_GRANTED) {
-//
-//            // Should we show an explanation?
-//            if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) getApplicationContext(),
-//                    Manifest.permission.INTERNET)) {
-//
-//                // Show an explanation to the user *asynchronously* -- don't block
-//                // this thread waiting for the user's response! After the user
-//                // sees the explanation, try again to request the permission.
-//
-//            } else {
-//
-//                // No explanation needed, we can request the permission.
-//
-//                ActivityCompat.requestPermissions((Activity) getApplicationContext(),
-//                        new String[]{Manifest.permission.READ_CONTACTS},
-//                        1);
-//
-//                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-//                // app-defined int constant. The callback method gets the
-//                // result of the request.
-//            }
-//        }
-
+        new connectServer().start();
+        adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, lsMess);
+        lvMess.setAdapter(adapter);
+        Intent intent = getIntent();
+        name = intent.getStringExtra("name");
 
     }
 
     private void mapped() {
-        txtMess = (TextView) findViewById(R.id.txtMess);
+        txtName = (TextView) findViewById(R.id.txtName);
         edtMess = (EditText) findViewById(R.id.edtInputMess);
         btnSubmit = (Button) findViewById(R.id.btnsubmit);
+        lvMess = (ListView) findViewById(R.id.listViewMess);
+
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.btnsubmit:
                 new putmess().start();
+                break;
+            case R.id.txtName:
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                new connect().execute();
+                Toast.makeText(this, "Reconnect Server", Toast.LENGTH_SHORT).show();
                 break;
         }
     }
 
-    private class putmess extends Thread{
+    private class putmess extends Thread {
         @Override
         public void run() {
             try {
                 Log.d(TAG, "putting mess");
                 String putMess = edtMess.getText().toString().trim();
-                os.write(putMess);
+                os.write(name + ": " + putMess);
                 os.newLine();
                 os.flush();
                 Log.d(TAG, "put mess conplete");
+                adapter.notifyDataSetChanged();
+
             } catch (Exception e) {
                 e.printStackTrace();
-                Log.d(TAG, "read mess\n"+e);
+                Log.d(TAG, "read mess\n" + e);
             }
 
         }
     }
 
-    private class connect extends AsyncTask<Void, Void, Void>{
+    private class connect extends AsyncTask<Void, String, Void> {
 
         @Override
         protected void onPreExecute() {
@@ -127,22 +125,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         @Override
         protected Void doInBackground(Void... voids) {
             try {
-                socket = new Socket("10.200.200.244", 3000);
+                socket = new Socket("192.168.1.10", 3000);
                 is = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 os = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                os.write("Userconnect: " + name);
+                os.newLine();
+                os.flush();
                 Log.d(TAG, "connect sucess");
                 String inMess;
-                while(true){
+                while (true) {
                     inMess = is.readLine();
-                    Log.d(TAG, "Server: " + inMess);
+                    publishProgress(inMess);
+
                 }
-            } catch (IOException e) {
-                Log.d(TAG, "error: "+ e);
+            } catch (Exception e) {
+                Log.d(TAG, "error: " + e);
             }
-
-
             return null;
+        }
 
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
 
         }
 
@@ -152,4 +156,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class connectServer extends Thread {
+        @Override
+        public void run() {
+            super.run();
+            try {
+                socket = new Socket("192.168.1.10", 3000);
+                is = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                os = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                os.write("Userconnect: " + name);
+                os.newLine();
+                os.flush();
+                Log.d(TAG, "connect sucess");
+                String inMess;
+                while (true) {
+                    inMess = is.readLine();
+                    lsMess.add(inMess);
+                    Toast.makeText(MainActivity.this, "New message: " + inMess, Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "Server: " + inMess);
+                    adapter.notifyDataSetChanged();
+                }
+            } catch (Exception e) {
+                Log.d(TAG, "error: " + e);
+            }
+        }
+    }
 }
